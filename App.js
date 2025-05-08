00748f906c35f6e5bc373c76bf331f1171f52815
Joona <t9hejo01@students.oamk.fi>
2025-05-08 16:12:01 +0300

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [backgroundColor, setBackgroundColor] = useState('black');
+  
+
+  const logPress = () => {
+    console.log('Button Pressed!')
+  }
+
+  const handlePress = () => {
+    setBackgroundColor('red');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+      <Button title="Press" color={backgroundColor} onPress={() => {logPress(); handlePress();}} />
       <StatusBar style="auto" />
     </View>
   );
